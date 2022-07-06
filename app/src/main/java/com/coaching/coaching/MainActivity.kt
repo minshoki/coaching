d1f2ce6121432f4cfac4f7cd263da26ee4717c1c
shoki <a01050335943@Gmail.com>
2022-07-06 12:24:18 +0900

    initial commit

--- app/src/main/java/com/coaching/coaching/MainActivity.kt
+++ app/src/main/java/com/coaching/coaching/MainActivity.kt
@@ -2,10 +2,33 @@ package com.coaching.coaching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.coaching.coaching.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initWebView()
+        binding.webview.loadUrl("http://sqrt5.iptime.org:8081")
+    }
+
+    private fun initWebView() {
+        with(binding.webview.settings) {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+
+            setSupportMultipleWindows(true)
+            builtInZoomControls = false
+            displayZoomControls = false
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            allowFileAccess = true
+            allowContentAccess = true
+        }
     }
 }
